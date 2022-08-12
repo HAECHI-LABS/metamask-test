9409c07f165d2178509b5caebd1dfd08ae0ceb2d
borre <borre@haechi.io>
2022-08-12 16:38:34 +0900

    update address and balance

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import MetaMaskOnboarding from '@metamask/onboarding';
 import { useEffect, useState } from 'react';
+import { providers } from 'ethers';
 import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
@@ -9,23 +10,74 @@ function App() {
     new MetaMaskOnboarding()
   );
   const [provider, setProvider] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [accounts, setAccounts] = useState([]);
+  const [balance, setBalance] = useState(null);
+  
+  async function detectLoggedIn() {
+    try {
+      const ethersProvider = new providers.Web3Provider(provider, 'any');
+      const signer = await ethersProvider.getSigner();
+      await signer.getAddress();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 
   useEffect(() => {
-    detectEthereumProvider().then(setProvider)
+    detectEthereumProvider().then(provider => {
+      setProvider(provider);
+      detectLoggedIn().then(setLoggedIn);
+      provider.on("accountsChanged", accounts => {
+        if (accounts.length === 0) {
+          setLoggedIn(false);
+        } else {
+          setLoggedIn(true);
+        }
+      })
+    })
     .catch(err => console.log(err));
   }, []);
+
+  useEffect(() => {
+    async function doWork() {
+      if (loggedIn === false) {
+        return;
+      }
+      const ethersProvider = new providers.Web3Provider(provider, 'any');
+      const signer = await ethersProvider.getSigner();
+
+      // const accounts = await provider.request({ method: 'eth_requestAccounts' });
+      const address = await signer.getAddress();
+      setAccounts([address]);
+      const balance = await signer.getBalance();
+      setBalance(balance.toString());  
+    }
+    doWork();
+  }, [loggedIn])
 
   function startOnboarding() {
     console.log("start onboarding");
     onboarding.startOnboarding();
   }
 
-  // mobile onboarding
-  // metamask가 설치되어있는지 확인하기.
-
-  // desktop onboarding
-
-  console.log("metamask installed", MetaMaskOnboarding.isMetaMaskInstalled());
+  function connect() {
+    provider
+      .request({ method: 'eth_requestAccounts' })
+      .then(() => {
+        setLoggedIn(true);
+      })
+      .catch((err) => {
+        if (err.code === 4001) {
+          // EIP-1193 userRejectedRequest error
+          // If this happens, the user rejected the connection request.
+          console.log('Please connect to MetaMask.');
+        } else {
+          console.error(err);
+        }
+      });
+  }
 
   return (
     <div className="App">
@@ -36,15 +88,19 @@ function App() {
         <p>
           Metamask is installed: {"" + MetaMaskOnboarding.isMetaMaskInstalled()}
         </p>
-        {MetaMaskOnboarding.isMetaMaskInstalled() === false && <button onClick={startOnboarding}>start onboarding</button>}
-        <p>
-          provider: {provider ? "exist" : "not exist"}
-        </p>
+        {MetaMaskOnboarding.isMetaMaskInstalled() === false && <button onClick={startOnboarding}>start install in Desktop</button>}
+        {MetaMaskOnboarding.isMetaMaskInstalled() === false && <a href={"https://metamask.app.link/dapp/haechi-labs.github.io/metamask-test/"}>install metamask in mobile</a>}
+  
         {provider && <p>
-          connected: {"" + provider.isConnected()} <br/>
-          isMetaMask: {"" + provider.isMetaMask}
+          isMetaMask: {"" + provider.isMetaMask} <br />
+          loggedIn: {"" + loggedIn} <br />
+          {loggedIn === false && <button onClick={connect}>connect metamask</button>}
+<br/>
+          {loggedIn && <span>accounts: {accounts.join(', ')}</span>}
+          <br/>
+          {loggedIn && <span>balance: {balance}</span>}
           </p>}
-        <p>metamask deeplink: 메타마스크가 설치되어있으면 메타마스크 안에서 DApp이 열립니다. 설치되어있지 않다면 metamask 설치 페이지로 이동됩니다. <a href="https://metamask.app.link/dapp/haechi-labs.github.io/metamask-test/">https://metamask.app.link/dapp/haechi-labs.github.io/metamask-test/</a></p>
+        
       </header>
     </div>
   );
