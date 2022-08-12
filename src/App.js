96a9c5b8b501288c271cc752849ce9e93b7edc20
borre <borre@haechi.io>
2022-08-12 15:36:26 +0900

    add deeplink test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import MetaMaskOnboarding from '@metamask/onboarding';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
   const [onboarding, setOnboarding] = useState(
     new MetaMaskOnboarding()
   );
+  const [provider, setProvider] = useState(null);
+
+  useEffect(() => {
+    detectEthereumProvider().then(setProvider)
+    .catch(err => console.log(err));
+  }, []);
 
   function startOnboarding() {
     console.log("start onboarding");
     onboarding.startOnboarding();
-
   }
+
+  // mobile onboarding
+  // metamask가 설치되어있는지 확인하기.
+
+  // desktop onboarding
+
+  console.log("metamask installed", MetaMaskOnboarding.isMetaMaskInstalled());
 
   return (
     <div className="App">
@@ -20,7 +33,18 @@ function App() {
         <p>
           Metamask test
         </p>
-        <button onClick={startOnboarding}>start onboarding</button>
+        <p>
+          Metamask is installed: {"" + MetaMaskOnboarding.isMetaMaskInstalled()}
+        </p>
+        {MetaMaskOnboarding.isMetaMaskInstalled() === false && <button onClick={startOnboarding}>start onboarding</button>}
+        <p>
+          provider: {provider ? "exist" : "not exist"}
+        </p>
+        {provider && <p>
+          connected: {"" + provider.isConnected()} <br/>
+          isMetaMask: {"" + provider.isMetaMask}
+          </p>}
+        <p>metamask deeplink: 메타마스크가 설치되어있으면 메타마스크 안에서 DApp이 열립니다. 설치되어있지 않다면 metamask 설치 페이지로 이동됩니다. <a href="https://metamask.app.link/dapp/haechi-labs.github.io/metamask-test/">https://metamask.app.link/dapp/haechi-labs.github.io/metamask-test/</a></p>
       </header>
     </div>
   );
